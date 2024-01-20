28025cf2e96596b295dde4765691e864efd56016
Andras Serfozo <andras@xploro.health>
2024-01-20 11:13:57 +0000

    Make it work on mobile again

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import Webcam from "react-webcam"
-import useWindowDimensions from "./hooks/window-dimensions"
-// import { useEffect } from "react"
+// import useWindowDimensions from "./hooks/window-dimensions";
+import { useEffect } from "react";
+
+const { innerWidth: width, innerHeight: height } = window
 
 function App() {
-  const { height, width } = useWindowDimensions()
+  // const { height, width } = useWindowDimensions();
 
-  // useEffect(() => {
-  //   // @ts-ignore
-  //   screen.orientation?.lock("any")
-  // }, [])
-
+  useEffect(() => {
+    // @ts-ignore
+    screen.orientation.lock("any")
+  }, [])
+  
   return (
     <Webcam
       videoConstraints={{ facingMode: "environment" }}
